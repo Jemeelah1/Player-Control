d4c2ab156dfba6b367dcf772168a6162fea28a68
Jemila Ibrahim D. <83028679+Jemeelah1@users.noreply.github.com>
2024-09-06 12:43:57 +0100

    Update SpinPropellerX.cs

--- Challenge/SpinPropellerX.cs
+++ Challenge/SpinPropellerX.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpinPropellerX : MonoBehaviour
 {
     public float spinSpeed = 1000.0f;  // Speed at which the propeller spins
+    public float rotationSpeed;
+    public float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,11 @@ public class SpinPropellerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        verticalInput = Input.GetAxis("vertical");
         // Rotate the propeller around its local Z axis (forward axis)
         transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+
+        // Rotate the propeller around its local Z axis (forward axis)
+        transform.Rotate(Vector3.right, rotationSpeed * verticalInput * Time.deltaTime);
     }
 }
